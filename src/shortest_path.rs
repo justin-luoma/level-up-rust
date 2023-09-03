2de74b27b4e58f5c8a3ba6c73981da7876279a35
Justin Luoma <me@justin.rs>
2023-09-03 09:50:41 -0400

    feat: complete 16, shortest path

--- src/shortest_path.rs
+++ src/shortest_path.rs
@@ -1,4 +1,5 @@
-use std::collections::{HashMap, HashSet};
+use std::cmp::Ordering;
+use std::collections::{BinaryHeap, HashMap, HashSet};
 
 type Node = usize;
 type Cost = usize;
@@ -31,9 +32,74 @@ impl Graph {
     }
 }
 
+#[derive(Debug, Clone, Eq)]
+struct Visit {
+    distance: Cost,
+    position: Node,
+    history: Vec<Node>,
+}
+
+impl Visit {
+    fn new(position: Node, distance: Cost, history: Vec<Node>) -> Self {
+        Self {
+            distance,
+            position,
+            history
+        }
+    }
+
+    fn from_start(start: Node) -> Self {
+        Visit::new(start, 0, Default::default())
+    }
+}
+
+impl PartialOrd<Self> for Visit {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Visit {
+    fn cmp(&self, other: &Self) -> Ordering {
+        other.distance.cmp(&self.distance)
+    }
+}
+
+impl PartialEq for Visit {
+    fn eq(&self, other: &Self) -> bool {
+        self.distance.eq(&other.distance)
+    }
+}
 
 fn shortest_path(g: &Graph, start: Node, goal: Node) -> Option<(Vec<Node>, Cost)> {
-    todo!()
+    let mut distances = HashMap::from([(start, 0)]);
+    let mut visit = BinaryHeap::new();
+    visit.push(Visit::from_start(start));
+
+    while let Some(Visit { distance, position, mut history}) = visit.pop() {
+        if position == goal {
+            history.push(goal);
+            return Some((history, distance));
+        }
+
+        if let Some(neighbors) = g.edges.get(&position) {
+            for (neighbor, cost) in neighbors {
+                let new_distance = distance + cost;
+                let is_shorter = distances
+                    .get(neighbor)
+                    .map_or(true, |&current| new_distance < current);
+
+                if is_shorter {
+                    let mut next = Visit::new(*neighbor, new_distance, history.clone());
+                    next.history.push(position);
+                    visit.push(next);
+                    distances.insert(*neighbor, new_distance);
+                }
+            }
+        }
+    }
+
+    None
 }
 
 #[cfg(test)]
